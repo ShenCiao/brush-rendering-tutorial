1d477d85f25302a18887bbbafe235fffbfdc4bfc
ShenCiao <3294833183@qq.com>
2023-09-21 10:34:25 +0800

    GlslEditor.tsx

---
+++ src/components/GlslEditor.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {conf, language} from './glsl';
+import { Editor } from "@monaco-editor/react";
+import type {EditorProps} from "@monaco-editor/react";
+
+export function GlslEditor(props:Omit<EditorProps, 'defaultLanguage'|'language'>){
+    const handleEditorDidMount = (editor, monaco) => {
+        monaco.languages.register({ id: 'glsl' });
+        monaco.languages.setMonarchTokensProvider('glsl', language);
+        monaco.languages.setLanguageConfiguration('glsl', conf);
+        if(typeof props.onMount === 'function'){
+            props.onMount(editor, monaco);
+        }
+    };
+    return (
+        <Editor
+            {...props}
+            defaultLanguage="glsl"
+            onMount={handleEditorDidMount}
+        />
+    );
+}
