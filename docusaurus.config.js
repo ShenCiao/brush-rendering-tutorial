43e3953faa8c959580b328e23c44872f7d80510f
ShenCiao <3294833183@qq.com>
2024-01-24 19:48:46 +0800

    fixing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ export default {
   url: 'https://anonymous.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/brush-rendering-tutorial/',
+  baseUrl: '',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
